c71de9b9a0039636a0d9de91e09a68ad11280dc9
juanillo <juanillojohnmichael7@gmail.com>
2024-07-02 15:45:01 +0800

    footer

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+var slider = tns({
+    container: ".testimonial__slider",
+    items: 3,
+    speed: 400,
+    gutter:100,
+    slideBy: "page",
+    autoplay: true,
+    navPosition: "buttom",
+    containerControls: "#controls",
+    prevButton: ".prev",
+    nextButton: ".next",
+});
